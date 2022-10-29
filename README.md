de30618c1d22a76632566b484afe75299706ba12
Zandarh <ubaksxander@gmail.com>
2022-10-29 04:55:01 +0100

    Tasks

---
+++ README.md
@@ -0,0 +1,10 @@
+# Task Description
+
+- Setup a server (Hosted) -- Create an (GET) api endoint that returns the following json response:
+
+- { "slackUsername": String, "backend": Boolean, "age": Integer, "bio": String }
+
+   - SlackUsername should be a **string** datatype and your slack username
+   - Backend should be a **boolean** datatype
+     - Age should be an  **integer** datatype
+       - Bio(description about yourself) should be a **string** datatype
---
+++ server.py
@@ -0,0 +1,28 @@
+# import main Flask class, then jsonigy and requet object
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from werkzeug.exceptions import MethodNotAllowed
+
+
+# create the flask app
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/", strict_slashes=False)
+def my_data():
+    """Returns my data"""
+    # checking if the http method is Get
+    if request.method == "GET":
+        my_dict = {
+            "slackUsername": "zandarh",
+            "backend": True,
+            "age": 30,
+            "bio": "I am an aspiring software engineer"
+            }
+        return jsonify(my_dict)
+    else:
+        raise MethodNotAllowed
+
+if __name__ == "__main__":
+        app.run(debug=True)
